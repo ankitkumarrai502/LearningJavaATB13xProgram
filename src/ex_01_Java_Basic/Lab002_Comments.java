942812013e797b6d6a77b5e5977c895f968f9f3c
Ankit Rai <ankitkumarrai502@gmail.com>
2025-06-18 01:20:23 +0530

    18th June

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+//        System.out.println("Hello Test");   This is a single line comment
+
+
+    }/*System.out.println("hi");
+        System.out.println("hello");
+        System.out.println("tata");
+        System.out.println("bye");*//*  This is a multi line comment*/
+
+  /*  Name : Ankit Kumar Rai
+    Learning Purpose : To become a succesfull Automation Test Engineer
+    This is a documentation comment and this is a perfect use of multiline commnents*/
+}
