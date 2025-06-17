f67109940830c6b1436f64da7e261983a15fe830
Ankit Rai <ankitkumarrai502@gmail.com>
2025-06-18 01:58:24 +0530

    18th June

---
+++ src/ex_02_Java_Basics/comments.java
@@ -0,0 +1,7 @@
+package ex_02_Java_Basics;
+
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("Today I learned deeply about comments in Java Language");
+    }
+}
