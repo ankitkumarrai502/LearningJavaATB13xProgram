4f656aa3dc9065a0f738f2f2d47945fc0377d17e
Ankit Rai <ankitkumarrai502@gmail.com>
2025-06-26 00:10:49 +0530

    25th june

--- src/ex_01_Java_Basic/Lab004_Comments_SingleLine.java
+++ src/ex_01_Java_Basic/Lab004_Comments_SingleLine.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class Lab004_Comments {
+public class Lab004_Comments_SingleLine {
     public static void main(String[] args) {
         // This is a comment, this code will nto be executed.
          /*System.out.println("Hi");
